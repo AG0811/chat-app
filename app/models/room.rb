95841ae1dcaeb2065ebd5638b627f25537f878a2
AG0811 <ag114126+d@gmail.com>
2024-05-09 15:08:57 +0900

    チャットルーム作成機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,7 @@
 class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
+
+  # バリデーション：ルーム名が存在(presence)の場合のみ作成可(true)
+  validates :name, presence: true
 end
